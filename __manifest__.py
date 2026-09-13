f8981c2d6677fef633636a94351d018df57446a3
IbrahemIsbieh <abrahemsbaih3@gmail.com>
2026-09-13 12:50:30 +0300

    Add basic ORM operations for library books

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     'name' : 'Library Management',
     'version': '19.0.1.0.0',
-    'summary': 'Manage Car Repairs and Workshop Operations',
+    'summary': 'Manage library books and borrowing operations',
     'depends': ['base' ,'mail'],
     'data': [
 
--- models/library_book.py
+++ models/library_book.py
@@ -9,6 +9,7 @@ class LibraryBook(models.Model):
     publication_date = fields.Date(string='Publication Date')
     state = fields.Selection(
         [
+            ('draft', 'Draft'),
             ('available', 'Available'),
             ('borrowed', 'Borrowed'),
             ('lost', 'Lost'),
@@ -17,3 +18,17 @@ class LibraryBook(models.Model):
         default='available',
         tracking=True
     )
+    def get_available_books(self):
+       books = self.env['library.book'].search([('state', '=', 'available')])
+       return books
+    def create_books(self):
+       new_books= self.env['library.book'].create({'name':'Clean Code','author':'Robert Martin','isbn':'9780132350884'})
+       return new_books
+
+    def borrow_book(self):
+        book =self.env['library.book'].search([('name', '=', 'Clean Code')], limit=1)
+        book.write({'state':'borrowed'})
+
+    def delete_book(self):
+        book=self.env['library.book'].search([('name', '=', 'Clean Code')], limit=1)
+        book.unlink()
